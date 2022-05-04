c6cc5226664a68e5032199539660986f5abbb62a
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-05-04 13:20:09 -0300

    base setup

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import GlobalStyle from "./components/Normalize"
+
+import Login from "./components/Login"
+import Register from "./components/Register"
+import Home from "./components/Home"
+import CashIn from "./components/CashIn"
+import CashOut from "./components/Cashout"
+
+import UserContext from "./contexts/UserContext"
+
+export default function App() {
+    const [userInfo, setUserInfo] = React.useState({
+        name: "",
+        image: "",
+        email: "",
+        token: "",
+        percentage: "",
+    })
+
+    const totalInfo = {userInfo, setUserInfo};
+    
+    return (
+        <UserContext.Provider value={totalInfo}>
+            <BrowserRouter>
+                <GlobalStyle />
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/cashin" element={<CashIn />} />
+                    <Route path="/cashout" element={<CashOut />} />
+                </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
+    )
+}
---
+++ src/components/Login.js
@@ -0,0 +1,95 @@
+import React from "react";
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useContext } from "react";
+
+export default function Login() {
+    const URL = "INSIRA URL AQUI"
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const {userInfo, setUserInfo} = useContext(UserContext);
+    const [disabled, setDisabled] = useState(false);
+
+    const navigator = useNavigate();
+
+    function blockForValidation(e) {
+        e.preventDefault();
+        setDisabled(true);
+        validateLogin();
+    }
+
+    function validateLogin() {
+
+    }
+
+
+    return (
+        <Menu>
+            <h1>MyWallet</h1>
+
+            <form onSubmit={blockForValidation} style={disabled ? {opacity: '0.5'} : {}} disabled={disabled ? "disabled" : ""}>
+                <input required placeholder="E-mail" type="email" onChange={e => setEmail(e.target.value)}></input>
+                <input required placeholder="Senha" type="password" onChange={e => setPassword(e.target.value)}></input>
+                <button type="submit">Entrar</button>
+            </form>
+
+            <Link to="/register">Primeira vez? Cadastre-se!</Link>
+        </Menu>
+    )
+}
+
+const Menu = styled.main`
+width: 100vw;
+height: 100vh;
+
+background: #8C11BE;
+display: flex;
+align-items: center;
+flex-direction: column;
+
+h1 {
+    font-family: 'Saira Stencil One', cursive;
+    color: #126BA5;
+    font-size: 32px;
+    font-weight: 400;
+}
+
+a {
+    color: #FFFFFF;
+    font-size: 15px;
+}
+
+form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+input {
+    margin-bottom: 10px;
+    width: 326px;
+    height: 60px;
+    border: 1px solid #FFFFFF;
+    border-radius: 5px;
+    padding: 10px;
+    background: #FFFFFF;
+}
+
+input::placeholder {
+    color: #000000;
+}
+
+button {
+    width: 326px;
+    height: 60px;
+    border: none;
+    background: #A328D6;
+    border-radius: 5px;
+    padding: 5px;
+
+    color: #FFFFFF;
+}
+`
